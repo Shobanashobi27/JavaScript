d3a022ccd9186ca9807ff3fd03cee73ad8258bc8
Samyuktha <Shobanashobi273@gmail.com>
2025-12-11 15:07:21 +0530

    m

---
+++ Module.js
@@ -0,0 +1,28 @@
+//let ipl=["mi","rbc","kkr","csk","lsg","dc","gt","rr"];
+//let product ={
+  //  name: "iphone",
+    //price: 500000,
+     //model:"iphone 13",
+    //rating :4.5,
+    //stock:1500,
+//};
+
+//let{model,stock}=product;
+
+//console.log(product.model);
+
+//console.log(model);
+//console.log(stock);
+
+//let{stock,model}=product;
+
+//let [a,paltans,srk]=ipl;
+//console.log(a,paltans);
+//console.log(srk);
+
+import{str} from "./storage.js";
+import{obj} from "./storage.js";
+
+
+console.log(str);
+console.log(obj);
---
+++ storage.js
@@ -0,0 +1,14 @@
+//let str="i am String from storage file";
+
+//export let str="iam string from storage file";
+
+//export let obj ={
+ // name:"DHONI",
+ // age:"41",
+ // role:"wk",
+ // alsoKnownas:"thala"
+
+
+//};
+
+export let movies=["dude","lokah","bison"];
